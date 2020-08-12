7f51cdf749a413cbecec4686b499e6fd35ae5a34
Denis Malyshev <mdvlift@gmail.com>
2020-08-12 22:55:10 +0400

    Task 2(1)

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,47 +1,35 @@
-const arrowPrev = document.getElementsByClassName("slider__arrow slider__arrow_prev");
-const arrowNext = document.getElementsByClassName("slider__arrow slider__arrow_next");
+const arrowPrev = document.getElementsByClassName(
+  "slider__arrow slider__arrow_prev"
+);
+const arrowNext = document.getElementsByClassName(
+  "slider__arrow slider__arrow_next"
+);
 const sliderItem = document.querySelectorAll(".slider__item");
 
 let arrSliderItem = Array.from(sliderItem);
-let index;
-
-const sliderDot = document.querySelectorAll(".slider__dot");
-let arrSliderDot = Array.from(sliderDot);
+let indexActiv;
 
 function searchActiveSlide() {
-  for (i = 0; i < arrSliderItem.length; i++) {
-    if (arrSliderItem[i].className === "slider__item slider__item_active") {
-      index = i;
-    }
-  }
+  indexActiv = arrSliderItem.findIndex(function (element) {
+    return element.className === "slider__item slider__item_active"
+      ? true
+      : false;
+  });
+  arrSliderItem[indexActiv].className = "slider__item";
 }
 
 arrowPrev[0].onclick = function () {
   searchActiveSlide();
-  if (index === 0) {
-    arrSliderItem[index].className = "slider__item";
-    arrSliderDot[index].className = "slider__dot";
-    arrSliderItem[arrSliderItem.length - 1].className = "slider__item slider__item_active";
-    arrSliderDot[arrSliderItem.length - 1].className = "slider__dot slider__dot_active";
-  } else {
-    arrSliderItem[index].className = "slider__item";
-    arrSliderItem[index - 1].className = "slider__item slider__item_active";
-    arrSliderDot[index].className = "slider__dot";
-    arrSliderDot[index - 1].className = "slider__dot slider__dot_active";
-  }
+  indexActiv === 0
+    ? (arrSliderItem[arrSliderItem.length - 1].className =
+        "slider__item slider__item_active")
+    : (arrSliderItem[indexActiv - 1].className =
+        "slider__item slider__item_active");
 };
 
 arrowNext[0].onclick = function () {
-    searchActiveSlide();
-  if ((arrSliderItem.length - 1) === index) {
-    arrSliderItem[index].className = "slider__item";
-    arrSliderDot[index].className = "slider__dot";
-    arrSliderItem[0].className = "slider__item slider__item_active";
-    arrSliderDot[0].className = "slider__dot slider__dot_active";
-  } else {
-    arrSliderItem[index].className = "slider__item";
-    arrSliderDot[index].className = "slider__dot";
-    arrSliderItem[index + 1].className = "slider__item slider__item_active";
-    arrSliderDot[index + 1].className = "slider__dot slider__dot_active";
-  }
-}
+  searchActiveSlide();
+  indexActiv === (arrSliderItem.length - 1)
+    ? (arrSliderItem[0].className = "slider__item slider__item_active")
+    : (arrSliderItem[indexActiv + 1].className = "slider__item slider__item_active");
+};
