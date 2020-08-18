8f7c5359feb39486ceb6aecf7793f56ff3e33ae9
Denis Malyshev <mdvlift@gmail.com>
2020-08-18 18:26:40 +0400

    Task 3(1)

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,10 +1,14 @@
 const dropdownValue = document.querySelectorAll(".dropdown__value");
 const dropdownList = document.querySelectorAll(".dropdown__list");
 const dropdownItem = document.querySelectorAll(".dropdown__item");
-let dropdownLink = document.querySelectorAll(".dropdown__link");
+const dropdownLink = document.querySelectorAll(".dropdown__link");
+let index;
 
-dropdownLink.forEach((e) => e.addEventListener("click", function (e) { 
-    e.preventDefault();
+dropdownLink.forEach((e) =>
+  e.addEventListener(
+    "click",
+    function (e) {
+      e.preventDefault();
     },
     false
   )
@@ -13,18 +17,20 @@ dropdownLink.forEach((e) => e.addEventListener("click", function (e) {
 dropdownValue.forEach((eValue, i) => {
   eValue.addEventListener("click", function () {
     dropdownList[i].classList.toggle("dropdown__list_active");
+    index = i;
+  });
+});
 
-    dropdownList.forEach((eList) => {
-      let dropdownItem = eList
-        .closest(".dropdown__list")
-        .querySelectorAll(".dropdown__item");
-        
-      dropdownItem.forEach((eItem) => {
-        eItem.addEventListener("click", function () {
-          eValue.textContent = eItem.textContent;
-          dropdownList[i].classList.remove("dropdown__list_active");
-        });
-      });
+dropdownList.forEach((eList) => {
+  let dropdownItem = eList
+    .closest(".dropdown__list")
+    .querySelectorAll(".dropdown__item");
+  let d = eList.closest(".dropdown").querySelector(".dropdown__value");
+
+  dropdownItem.forEach((eItem) => {
+    eItem.addEventListener("click", function () {
+      d.textContent = eItem.textContent;
+      dropdownList[index].classList.remove("dropdown__list_active");
     });
   });
 });
--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,13 +17,14 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-     */
+    document.addEventListener('keyup', (event) => {
+      let symbol = this.currentSymbol.textContent;
+      if (symbol.toLowerCase() === event.key.toLowerCase()){
+        this.success();
+      } else {
+        this.fail();
+      }
+    });
   }
 
   success() {
