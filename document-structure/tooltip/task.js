60a65c4a429f6a1c5ba7938ec6909706697bc8d6
Denis Malyshev <mdvlift@gmail.com>
2020-08-31 13:53:39 +0400

    Task 6(1)

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,15 +1,18 @@
-const hasTooltip = document.querySelectorAll('.has-tooltip');
+const hasTooltip = document.querySelectorAll(".has-tooltip");
 
-hasTooltip.forEach(element =>{
-    element.insertAdjacentHTML('beforeEnd', `<div class="tooltip">${element.title}</div>`);
+hasTooltip.forEach((element) => {
+  element.insertAdjacentHTML("beforeEnd", `<div class="tooltip">${element.title}</div>`);
+
+  element.addEventListener("click", (e) => {
+    e.preventDefault();
     
-    element.addEventListener('click', (e) =>{
-        e.preventDefault();
-
-        const tootlip = document.querySelectorAll('.tooltip');
-        tootlip.forEach(e => e.classList.remove('tooltip_active'));
-
-        element.querySelector('.tooltip').classList.toggle('tooltip_active');
-    })
-    
-})
+    const tootlipElem = element.querySelector(".tooltip");
+    if (tootlipElem.classList.contains("tooltip_active")) {
+        tootlipElem.classList.remove("tooltip_active");
+    } else {
+      const tootlip = document.querySelectorAll(".tooltip");
+      tootlip.forEach((e) => e.classList.remove("tooltip_active"));
+      tootlipElem.classList.add("tooltip_active");
+    }
+  });
+});
