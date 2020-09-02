93d8fd28345616d7b7087ab01c0729295234d1b5
Denis Malyshev <mdvlift@gmail.com>
2020-09-02 20:32:25 +0400

    Task 7(2)

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,33 +1,16 @@
 const form = document.getElementById('form');
-const xhr = new XMLHttpRequest();
 const progress = document.getElementById( 'progress' );
 
-xhr.open('POST', form.action);
-
-const send = document.getElementById('send');
-
-send.onclick = function(e){
+form.addEventListener('submit', (e)=>{
     e.preventDefault();
-    
-    
-    xhr.onreadystatechange = function () {
-        xhr.onprogress= function(event){
-                let i = 0; 
-                console.log(event)
-                if(i == event.loaded){
-                    progress.value += 0.25;
-                    i+=event.total/4;
-                }
-        };
-    };    
-    xhr.send(formData);    
-}
+    const xhr = new XMLHttpRequest();
+    const formData = new FormData(form);
+    xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
 
+    xhr.onprogress = function(){
+        progress.value += 0.3;
+    }
 
+    xhr.send(formData);
+})
 
- 
-    
-          
-          
- 
-              
